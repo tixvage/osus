4bc2947ef341bce3ea7d7a5115e1ba2fffbb2cd7
ohasanov-hbrw <ka9an1sn00b@gmail.com>
2021-11-17 19:51:07 +0300

    wtf, how does this work?

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,7 +1,10 @@
 #include "Game.h"
 #include <algorithm>
+#include <cstdint>
 #include <iostream>
+#include <iomanip>
 #include <vector>
+#include <math.h>
 
 void Game::init(GameFile gf){
 	InitWindow(1280,960,"osus?");
@@ -23,18 +26,37 @@ void Game::update(){
 void Game::render(){
 	BeginDrawing();
 	ClearBackground(BLACK);
+	DrawFPS(10, 10);
 	//DrawCircle(GetMouseX(), GetMouseY(), 10, RED);
 	float scale = 0.6f;
-	float approachScale = scale*5.0f;
+	
+	int preempt;
+	int fade_in;
+	
+	if(std::stoi(gameFile.configDifficulty["ApproachRate"]) < 5){
+		preempt = 1200 + 600 * (5 - std::stoi(gameFile.configDifficulty["ApproachRate"])) / 5;
+		fade_in = 800 + 400 * (5 - std::stoi(gameFile.configDifficulty["ApproachRate"])) / 5;
+	}
+	else if(std::stoi(gameFile.configDifficulty["ApproachRate"]) < 5){
+		preempt = 1200 - 750 * (std::stoi(gameFile.configDifficulty["ApproachRate"]) - 5) / 5;
+		fade_in = 800 - 500 * (std::stoi(gameFile.configDifficulty["ApproachRate"]) - 5) / 5;
+	}
+	else{
+		preempt = 1200;
+		fade_in = 800;
+	}
+
 	DrawTextureEx(cursor, Vector2{GetMouseX()-cursor.width*scale*0.5f,GetMouseY()-cursor.height*scale*0.5f},0,scale, WHITE);
 	int size = gameFile.hitObjects.size();
 	for(int i = size-1; i >= 0; i--){
-		if(gameFile.hitObjects[i].time<currentTime*1000){
-			DrawTextureEx(hitCircle, Vector2{gameFile.hitObjects[i].x-hitCircle.width*0.5f,gameFile.hitObjects[i].y-hitCircle.height*0.5f},0,1, GREEN);
-			DrawTextureEx(hitCircleOverlay, Vector2{gameFile.hitObjects[i].x-hitCircleOverlay.width*0.5f,gameFile.hitObjects[i].y-hitCircleOverlay.height*0.5f},0,1, WHITE);
-			DrawTextureEx(approachCircle, Vector2{gameFile.hitObjects[i].x-approachCircle.width*approachScale*0.5f,gameFile.hitObjects[i].y-approachCircle.height*approachScale*0.5f},0,approachScale, Fade(WHITE, 0.2f));
+		if(gameFile.hitObjects[i].time - preempt <= currentTime*1000 and gameFile.hitObjects[i].time > currentTime*1000){
+			float approachScale = 2*(1-(currentTime*1000 - gameFile.hitObjects[i].time + preempt)/preempt)+1;
+			float clampedFade = (currentTime*1000 - gameFile.hitObjects[i].time + fade_in) / fade_in;
+			DrawTextureEx(hitCircle, Vector2{gameFile.hitObjects[i].x*2-hitCircle.width*0.5f,gameFile.hitObjects[i].y*2-hitCircle.height*0.5f},0,1, Fade(PINK, clampedFade));
+			DrawTextureEx(hitCircleOverlay, Vector2{gameFile.hitObjects[i].x*2-hitCircleOverlay.width*0.5f,gameFile.hitObjects[i].y*2-hitCircleOverlay.height*0.5f},0,1, Fade(WHITE, clampedFade));
+			DrawTextureEx(approachCircle, Vector2{gameFile.hitObjects[i].x*2-approachCircle.width*approachScale*0.5f,gameFile.hitObjects[i].y*2-approachCircle.height*approachScale*0.5f},0,approachScale, Fade(WHITE, clampedFade));
 		}
-		else break;
+		//else break;
 	}
 	
 	//DrawTexture(cursor,GetMouseX(), GetMouseY(), RED);
