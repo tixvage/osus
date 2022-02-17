f1646dad18875868a0ca6ce3d58862eca3f54476
ohasanov-hbrw <ka9an1sn00b@gmail.com>
2022-02-17 20:31:43 +0300

    melih bruh

--- lesgo.sh
+++ lesgo.sh
@@ -1,6 +1,8 @@
 echo "Github Automation";
+echo "pls commit message";
+read var1;
 git pull origin master;
 git add .;
-git commit -m "$1";
+git commit -m "$var1";
 git push origin master;
 echo "ins olmustur ne diim";
